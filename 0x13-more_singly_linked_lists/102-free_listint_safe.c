000b0f825179fef1c28dd5b06067bc7a4f26887e
emairu <idaehonemairuchinyeregift@gmail.com>
2022-10-25 16:06:04 -0700

    Free (safe version)

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -2,10 +2,11 @@
 
 /**
  * _ra - reallocates memory for an array of pointers
- to the nodes in a linked list
+ * to the nodes in a linked list
  * @list: the old list to append
  * @size: size of the new list (always one more than the old list)
  * @new: new node to add to the list
+ *
  * Return: pointer to the new list
  */
 listint_t **_ra(listint_t **list, size_t size, listint_t *new)
